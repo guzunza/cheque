6f87cd4b03934c9b41c0a3dcbfdfda4761fdb427
guzunza <zunzarren2020@gmail.com>
2024-11-30 21:36:36 -0300

    connection.js

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // Usuário MySQL
-    password: '', // Ou sua senha 
-    database: 'sistema_cheques', // Nome do banco
-    port: 3306 // Porta configurada no MySQL/XAMPP
+    host: process.env.DB_HOST || 'localhost', // Use DB_HOST do Render ou localhost
+    user: process.env.DB_USER || 'root',     // Use DB_USER do Render ou 'root'
+    password: process.env.DB_PASSWORD || 'Toby@2020', // Use DB_PASSWORD do Render ou sua senha
+    database: process.env.DB_NAME || 'sistema_cheques', // Use DB_NAME do Render ou seu banco de dados
+    port: process.env.DB_PORT || 3306  // Usar a porta do banco de dados, se necessário
 });
 
 connection.connect((err) => {
