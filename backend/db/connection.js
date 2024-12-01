909ddd9935d10d9b485518ceeb0ec6526016421c
guzunza <zunzarren2020@gmail.com>
2024-12-01 11:37:30 -0300

    connection.js

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,22 +1,18 @@
 const { Pool } = require('pg');
 
-// Configuração da conexão com o PostgreSQL
+// Configuração do banco de dados
 const pool = new Pool({
-  user: 'postgres', // Substitua pelo seu usuário do PostgreSQL
-  host: '127.0.0.1',   // Substitua pelo host do banco de dados
-  database: 'sistema_cheques', // Substitua pelo nome do banco de dados
-  password: 'Toby@2020', // Substitua pela sua senha
-  port: 5432,           // Porta padrão do PostgreSQL
+  user: 'sistema_cheques_3pno_user',        // Usuário do banco
+  host: 'dpg-ct66a156147c738027d0-a',      // Host do banco (Render)
+  database: 'sistema_cheques_3pno',        // Nome do banco
+  password: 'g8lYOUIMXATqWgDOzToCAaBhs5Yn4g6', // Senha do banco
+  port: 5432,                              // Porta padrão do PostgreSQL
+  ssl: {
+    rejectUnauthorized: false,             // Necessário para conexões seguras
+  },
 });
 
-// Teste da conexão
-pool.connect((err, client, release) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err.stack);
-  } else {
-    console.log('Conexão bem-sucedida ao PostgreSQL');
-    release(); // Libera o cliente de volta para o pool
-  }
-});
-
-module.exports = pool;
+// Testando a conexão
+pool.connect()
+  .then(() => console.log('Conexão com o banco de dados bem-sucedida!'))
+  .catch((err) => console.error('Erro ao conectar ao banco de dados:', err.stack));
